7bd158ebd04d58402a95867c979b6f988048d37c
Murilo Marinho <murilo@nml.t.u-tokyo.ac.jp>
2021-06-22 01:44:54 -0700

    [Python] Fixed the headers a bit.

--- src/rosilo_robot_kinematics/rosilo_robot_kinematics_interface.py
+++ src/rosilo_robot_kinematics/rosilo_robot_kinematics_interface.py
@@ -1,20 +1,20 @@
 """
 # Copyright (c) 2012-2021 Murilo Marques Marinho
 #
-#    This file is part of rosilo_robot_driver.
+#    This file is part of rosilo_robot_kinematics.
 #
-#    rosilo_robot_driver is free software: you can redistribute it and/or modify
+#    rosilo_robot_kinematics is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    rosilo_robot_driver is distributed in the hope that it will be useful,
+#    rosilo_robot_kinematics is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Lesser General Public License for more details.
 #
 #    You should have received a copy of the GNU Lesser General Public License
-#    along with rosilo_robot_driver.  If not, see <https://www.gnu.org/licenses/>.
+#    along with rosilo_robot_kinematics.  If not, see <https://www.gnu.org/licenses/>.
 #
 # ################################################################
 #
