538372ccdc57debe61838073e57eb9ccf9975d4c
qlin960618 <qlin1806@gmail.com>
2025-10-05 14:47:26 -0700

    added patch to fix dqrobotic tye issue

--- src/sas_robot_kinematics_py.cpp
+++ src/sas_robot_kinematics_py.cpp
@@ -34,20 +34,46 @@ using RKP = sas::RobotKinematicsProvider;
 
 PYBIND11_MODULE(_sas_robot_kinematics, m) {
 
+    py::object dq_py_class = py::module_::import("dqrobotics").attr("DQ");
+    auto get_py_dq_from_cpp = [dq_py_class](const DQ &dq_cpp)->py::object
+    {
+        return dq_py_class(dq_cpp.vec8());
+    };
+
+    auto get_cpp_dq_from_py = [dq_py_class](const py::object &pose_py)->DQ {
+        if (!py::isinstance(pose_py, dq_py_class)) {
+            throw std::runtime_error("Expected a dqrobotics.DQ object");
+        }
+        const VectorXd pose_vec = dq_py_class.attr("vec8")(pose_py).cast<VectorXd>();
+        return DQ(pose_vec);
+    };
+
+
     py::class_<RKI>(m, "RobotKinematicsInterface")
             .def(py::init<const std::string&>())
             .def("is_enabled",&RKI::is_enabled)
-            .def("get_pose",&RKI::get_pose)
-            .def("get_reference_frame",&RKI::get_reference_frame)
-            .def("send_desired_pose",&RKI::send_desired_pose)
+            .def("get_pose",[get_py_dq_from_cpp](const RKI &self)->py::object {
+                return get_py_dq_from_cpp(self.get_pose());
+            })
+            .def("get_reference_frame",[get_py_dq_from_cpp](const RKI &self)->py::object {
+                return get_py_dq_from_cpp(self.get_reference_frame());
+            })
+            .def("send_desired_pose",[get_cpp_dq_from_py](RKI &self, const py::object& pose_py) {
+                self.send_desired_pose(get_cpp_dq_from_py(pose_py));
+                }, py::arg("desired_pose"))
             .def("send_desired_interpolator_speed",&RKI::send_desired_interpolator_speed);
-
+            
     py::class_<RKP>(m, "RobotKinematicsProvider")
             .def(py::init<const std::string&>())
-            .def("get_desired_pose",&RKP::get_desired_pose)
+            .def("get_desired_pose",[get_py_dq_from_cpp](const RKP &self)->py::object {
+                return get_py_dq_from_cpp(self.get_desired_pose());
+            })
             .def("get_desired_interpolator_speed",&RKP::get_desired_interpolator_speed)
-            .def("is_enabled",&RKP::is_enabled)
-            .def("send_pose",&RKP::send_pose)
-            .def("send_reference_frame",&RKP::send_reference_frame);
+            .def("send_pose",[get_cpp_dq_from_py](RKP &self, const py::object& pose_py) {
+                self.send_pose(get_cpp_dq_from_py(pose_py));
+                }, py::arg("current_pose"))
+            .def("send_reference_frame",[get_cpp_dq_from_py](RKP &self, const py::object& pose_py) {
+                self.send_reference_frame(get_cpp_dq_from_py(pose_py));
+                }, py::arg("reference_frame"));
 
 }
